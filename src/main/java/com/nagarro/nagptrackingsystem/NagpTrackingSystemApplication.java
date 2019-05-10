c6c999e6f2cbb52520ad216163562d82e7f33bd0
Gaurav Kamboj <gauravkambojtu@gmail.com>
2019-05-10 15:01:52 +0530

    enabled CORS

--- src/main/java/com/nagarro/nagptrackingsystem/NagpTrackingSystemApplication.java
+++ src/main/java/com/nagarro/nagptrackingsystem/NagpTrackingSystemApplication.java
@@ -1,10 +1,16 @@
 package com.nagarro.nagptrackingsystem;
 
 import java.text.ParseException;
+import java.util.Arrays;
+import java.util.Collections;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
 
 @SpringBootApplication
 @EnableScheduling
@@ -12,6 +18,18 @@ public class NagpTrackingSystemApplication {
 
 	public static void main(String[] args) throws ParseException {
 		SpringApplication.run(NagpTrackingSystemApplication.class, args);
+	}
+
+	@Bean
+	public CorsFilter corsFilter() {
+		final UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+		final CorsConfiguration config = new CorsConfiguration();
+		config.setAllowCredentials(true);
+		config.setAllowedOrigins(Collections.singletonList("*"));
+		config.setAllowedHeaders(Arrays.asList("Origin", "Content-Type", "Accept"));
+		config.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "OPTIONS", "DELETE", "PATCH"));
+		source.registerCorsConfiguration("/**", config);
+		return new CorsFilter(source);
 	}
 
 }
